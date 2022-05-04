ff534ace6b24c9b70ef1aac42a76cf4ea165ad80
Trung Tín <s3752975@rmit.edu.vn>
2022-05-04 17:12:55 +0700

    second commit.

---
+++ Assignment2/Assignment2/src/test/java/com/assignment2/Assignment2/Assignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment2.Assignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
